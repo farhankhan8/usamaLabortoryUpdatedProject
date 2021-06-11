8327cf80e92fae9eb93b501edbefdf79c2e03d08
farhankhan8 <mfarhanahmed417@gmail.com>
2021-06-11 22:19:37 +0500

    UpdatedCode

---
+++ resources/views/admin/patient/multiple_reports.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.admin')
+@section('content')
+    <style>
+        @media print {
+            .noprint {
+                display: none;
+            }
+        }
+    </style>
+    <div class="card">
+        
+
+        @include("admin.TestPerformed.partial_patient")
+        @foreach($tests as $testPerformedsId)
+            @include("admin.TestPerformed.partial_report")
+        @endforeach
+
+        <div class="col-md-12 mb-12 noprint">
+            <button class="btn btn-primary" onclick="window.print()">Print Report</button>
+        </div>
+    </div>
+
+@endsection
